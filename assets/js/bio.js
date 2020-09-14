2b997cdf3cf6acd5e43a822e5f19c1d6e05f8694
el3ctricSol <aubrey.ky.young@gmail.com>
2020-09-14 16:15:49 -0700

    fixed mobile layout below the video

--- assets/js/bio.js
+++ assets/js/bio.js
@@ -10,30 +10,30 @@ var span = document.getElementsByClassName("close");
 // When the user clicks the button, open the modal 
 btn[0].onclick = function () {
   modal[0].style.display = "block";
-}
+};
 
 btn[1].onclick = function () {
   modal[1].style.display = "block";
-}
+};
 
 btn[2].onclick = function () {
   modal[2].style.display = "block";
-}
+};
 
 
 
 // When the user clicks on <span>, close the modal
 span[0].onclick = function () {
   modal[0].style.display = "none";
-}
+};
 
 span[1].onclick = function () {
   modal[1].style.display = "none";
-}
+};
 
 span[2].onclick = function () {
   modal[2].style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -46,4 +46,4 @@ window.onclick = function (event) {
   if (event.target == modal[2]) {
     modal[2].style.display = "none";
   }
-}
+};
