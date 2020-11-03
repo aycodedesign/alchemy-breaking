42f6b8a1269a7b3cb61116420d856e5a82482bbc
el3ctricSol <aubrey.ky.young@gmail.com>
2020-11-03 03:08:45 -0800

    finished Meet the Staff

--- assets/js/bio.js
+++ assets/js/bio.js
@@ -24,6 +24,10 @@ btn[3].onclick = function () {
   modal[3].style.display = "block";
 };
 
+btn[4].onclick = function () {
+  modal[4].style.display = "block";
+};
+
 // When the user clicks on <span>, close the modal
 span[0].onclick = function () {
   modal[0].style.display = "none";
@@ -41,18 +45,22 @@ span[3].onclick = function () {
   modal[3].style.display = "none";
 };
 
+span[4].onclick = function () {
+  modal[4].style.display = "none";
+};
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal[0]) {
-    modal[0].style.display = "none";
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  if (event.target == modal[1]) {
-    modal[1].style.display = "none";
-  }
-  if (event.target == modal[2]) {
-    modal[2].style.display = "none";
-  }
-  if (event.target == modal[3]) {
-    modal[3].style.display = "none";
-  }
+  // if (event.target == modal[1]) {
+  //   modal[1].style.display = "none";
+  // }
+  // if (event.target == modal[2]) {
+  //   modal[2].style.display = "none";
+  // }
+  // if (event.target == modal[3]) {
+  //   modal[3].style.display = "none";
+  // }
 };
